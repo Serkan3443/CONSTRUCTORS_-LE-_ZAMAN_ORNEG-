1cda26cf8f97ed55d866ae8a0d136d234b0d3658
Serkan <serkankabadayiogullari8233@gmail.com>
2023-08-23 22:16:26 +0300

    Constructors_ile_Zaman_Ornegi
    OOP, Constructors ile saat ve dakikayı kaç saniye olduklarını hesaplayan kodu yazdım

---
+++ CONSTRUCTORS_ZAMAN_ORNEGİ/CONSTRUCTORS_ZAMAN_ORNEGİ/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CONSTRUCTORS_ZAMAN_ORNEGİ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Time zaman=new Time(3800);
+            Console.WriteLine(zaman.ZamaniHesapla());
+
+          zaman=new Time("23:59:59");
+            Console.WriteLine(zaman.SureyiHesapla() + " saniye. ");
+
+        }
+    }
+}
---
+++ CONSTRUCTORS_ZAMAN_ORNEGİ/CONSTRUCTORS_ZAMAN_ORNEGİ/Time.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTRUCTORS_ZAMAN_ORNEGİ
+{
+    public class Time
+    {
+        int _duration; //saniye cinsinden
+        string _time;
+
+        public Time(int duration)
+        {
+            _duration = duration;
+        }
+        public Time(string time)
+        {
+            _time = time;
+        }
+
+        public string ZamaniHesapla()
+        {
+            int saat, dakika, saniye, saatsaniye, dakikasaniye;
+            saat = _duration / 3600;
+            saatsaniye = saat * 3600;
+            dakika = (_duration - saatsaniye) / 60;
+            if (dakika >= 60)
+                dakika = (_duration - saatsaniye) % 60;
+            dakikasaniye = dakika * 60;
+            saniye = _duration - saatsaniye - dakikasaniye;
+            return saat.ToString().PadLeft(2, '0') + ":" + dakika.ToString().PadLeft(2, '0') + ":" + saniye.ToString().PadLeft(2, '0');
+        }
+        public int SureyiHesapla()
+        {
+            int saatler = Convert.ToInt32(_time.Split(':')[0]);
+            int dakikalar = Convert.ToInt32(_time.Split(':')[1]);
+            int saniyeler = Convert.ToInt32(_time.Split(':')[2]);
+            return saatler * (int)Math.Pow(60, 2) + dakikalar * 60 + saniyeler;
+        }
+    }
+}
+
+
+
+
+
+
+
+
